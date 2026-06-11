83a0896d0ae3a18d2ace93d41f91e6b8cb46bea3
Sean Maas <seanmaas27@gmail.com>
2026-06-11 17:49:38 -0400

    Clean up I2C and stub camera reads

--- src/core/io/i2c.cpp
+++ src/core/io/i2c.cpp
@@ -25,76 +25,63 @@ void I2c::mcuInterrupt(uint32_t mask) {
         core.interrupts.sendInterrupt(ARM11, 0x71);
 }
 
-void I2c::writeBusData(int i, uint8_t value) {
-    // Write to one of the I2C_BUS_DATA registers
-    i2cBusData[i] = value;
-}
+uint8_t I2c::readMcu() {
+    // Get the MCU address and increment if enabled
+    uint8_t address = regAddrs[1];
+    regAddrs[1] += mcuInc;
 
-void I2c::writeBusCnt(int i, uint8_t value) {
-    // Write to one of the I2C_BUS_CNT registers and check some bits
-    i2cBusCnt[i] = value & 0x7F;
-    if (~value & BIT(7)) return; // Enable
-    if (value & BIT(1)) writeCount = 0; // Start
-
-    // Trigger an interrupt for the current bus if enabled
-    if (value & BIT(6)) {
-        uint8_t types[] = { 0x54, 0x55, 0x5C };
-        core.interrupts.sendInterrupt(ARM11, types[i]);
-    }
-
-    // Handle I2C reads if the direction is set to read
-    if (value & BIT(5)) {
-        switch ((i << 16) | (devAddr << 8) | ((regAddr += mcuInc) - mcuInc)) {
-            case 0x14B00: i2cBusData[i] = 0x13; return; // Version high
-            case 0x14B01: i2cBusData[i] = 0x41; return; // Version low
-            case 0x14B0B: i2cBusData[i] = 0x64; return; // Battery percent
-            case 0x14B0F: i2cBusData[i] = 0x02; return; // Power flags
-            case 0x14B10: i2cBusData[i] = readMcuIrqFlags(0); return;
-            case 0x14B11: i2cBusData[i] = readMcuIrqFlags(1); return;
-            case 0x14B12: i2cBusData[i] = readMcuIrqFlags(2); return;
-            case 0x14B13: i2cBusData[i] = readMcuIrqFlags(3); return;
-            case 0x14B18: i2cBusData[i] = readMcuIrqMask(0); return;
-            case 0x14B19: i2cBusData[i] = readMcuIrqMask(1); return;
-            case 0x14B1A: i2cBusData[i] = readMcuIrqMask(2); return;
-            case 0x14B1B: i2cBusData[i] = readMcuIrqMask(3); return;
-            case 0x14B30: i2cBusData[i] = readRtcValue(0); return;
-            case 0x14B31: i2cBusData[i] = readRtcValue(1); return;
-            case 0x14B32: i2cBusData[i] = readRtcValue(2); return;
-            case 0x14B33: i2cBusData[i] = readRtcValue(3); return;
-            case 0x14B34: i2cBusData[i] = readRtcValue(4); return;
-            case 0x14B35: i2cBusData[i] = readRtcValue(5); return;
-            case 0x14B36: i2cBusData[i] = readRtcValue(6); return;
-
-        default:
-            LOG_WARN("Unknown I2C bus %d read from device 0x%X, register 0x%X\n", i, devAddr, regAddr);
-            i2cBusData[i] = 0;
-            return;
-        }
-    }
-
-    // Set device and register addresses on the first two writes
-    i2cBusCnt[i] |= BIT(4); // Acknowledge
-    if (++writeCount == 1) {
-        devAddr = i2cBusData[i];
-        return;
-    }
-    else if (writeCount == 2) {
-        regAddr = i2cBusData[i];
-        if (i == 1 && (devAddr & 0xFE) == 0x4A) // MCU
-            mcuInc = (regAddr != 0x29 && regAddr != 0x2D && regAddr != 0x4F && regAddr != 0x61 && regAddr != 0x7F);
-        return;
-    }
-
-    // Handle I2C writes if the direction is set to write
-    switch ((i << 16) | (devAddr << 8) | ((regAddr += mcuInc) - mcuInc)) {
-        case 0x14A18: return writeMcuIrqMask(0, i2cBusData[i]);
-        case 0x14A19: return writeMcuIrqMask(1, i2cBusData[i]);
-        case 0x14A1A: return writeMcuIrqMask(2, i2cBusData[i]);
-        case 0x14A1B: return writeMcuIrqMask(3, i2cBusData[i]);
-        case 0x14A22: return writeMcuLcdPower(i2cBusData[i]);
+    // Read from an MCU register at the address
+    switch (address) {
+        case 0x00: return 0x13; // Version high
+        case 0x01: return 0x41; // Version low
+        case 0x0B: return 0x64; // Battery percent
+        case 0x0F: return 0x02; // Power flags
+        case 0x10: return readMcuIrqFlags(0);
+        case 0x11: return readMcuIrqFlags(1);
+        case 0x12: return readMcuIrqFlags(2);
+        case 0x13: return readMcuIrqFlags(3);
+        case 0x18: return readMcuIrqMask(0);
+        case 0x19: return readMcuIrqMask(1);
+        case 0x1A: return readMcuIrqMask(2);
+        case 0x1B: return readMcuIrqMask(3);
+        case 0x30: return readRtcValue(0);
+        case 0x31: return readRtcValue(1);
+        case 0x32: return readRtcValue(2);
+        case 0x33: return readRtcValue(3);
+        case 0x34: return readRtcValue(4);
+        case 0x35: return readRtcValue(5);
+        case 0x36: return readRtcValue(6);
 
     default:
-        LOG_WARN("Unknown I2C bus %d write to device 0x%X, register 0x%X\n", i, devAddr, regAddr);
+        // Catch reads from unknown MCU registers
+        LOG_WARN("Unknown I2C MCU read from register 0x%X\n", address);
+        return 0;
+    }
+}
+
+void I2c::writeMcu(uint8_t value) {
+    // Set the 8-bit MCU address and disable increment for certain values
+    if (writeCounts[1] == 2) {
+        regAddrs[1] = value;
+        mcuInc = (value != 0x29 && value != 0x2D && value != 0x4F && value != 0x61 && value != 0x7F);
+        return;
+    }
+
+    // Increment the MCU address if enabled
+    uint8_t address = regAddrs[1];
+    regAddrs[1] += mcuInc;
+
+    // Write to an MCU register at the address
+    switch (address) {
+        case 0x18: return writeMcuIrqMask(0, value);
+        case 0x19: return writeMcuIrqMask(1, value);
+        case 0x1A: return writeMcuIrqMask(2, value);
+        case 0x1B: return writeMcuIrqMask(3, value);
+        case 0x22: return writeMcuLcdPower(value);
+
+    default:
+        // Catch writes to unknown MCU registers
+        LOG_WARN("Unknown I2C MCU write to register 0x%X\n", address);
         return;
     }
 }
@@ -144,4 +131,82 @@ void I2c::writeMcuLcdPower(uint8_t value) {
     if (value & BIT(3)) mcuInterrupt(BIT(27)); // Bottom backlight on
     if (value & BIT(4)) mcuInterrupt(BIT(28)); // Top backlight off
     if (value & BIT(5)) mcuInterrupt(BIT(29)); // Top backlight on
+}
+
+uint8_t I2c::readCam(int i) {
+    // Increment the camera address and stub reads for now
+    uint16_t address = regAddrs[i / 2]++;
+    LOG_WARN("Unknown I2C camera %d read from register 0x%X\n", i, address);
+    return 0xFF;
+}
+
+void I2c::writeCam(int i, uint8_t value) {
+    // Set the 16-bit camera address across two writes
+    int j = i / 2;
+    if (writeCounts[j] == 2) {
+        regAddrs[j] = value;
+        return;
+    }
+    else if (writeCounts[j] == 3) {
+        regAddrs[j] = (regAddrs[j] << 8) | value;
+        return;
+    }
+
+    // Increment the camera address and stub writes for now
+    uint16_t address = regAddrs[j]++;
+    LOG_WARN("Unknown I2C camera %d write to register 0x%X\n", i, address);
+}
+
+void I2c::writeBusData(int i, uint8_t value) {
+    // Write to one of the I2C_BUS_DATA registers
+    i2cBusData[i] = value;
+}
+
+void I2c::writeBusCnt(int i, uint8_t value) {
+    // Write to one of the I2C_BUS_CNT registers and check some bits
+    i2cBusCnt[i] = value & 0x7F;
+    if (~value & BIT(7)) return; // Enable
+    if (value & BIT(1)) writeCounts[i] = 0; // Start
+
+    // Trigger an interrupt for the current bus if enabled
+    if (value & BIT(6)) {
+        uint8_t types[] = { 0x54, 0x55, 0x5C };
+        core.interrupts.sendInterrupt(ARM11, types[i]);
+    }
+
+    // Forward I2C reads to the addressed device
+    if (value & BIT(5)) { // Direction
+        switch ((i << 8) | devAddrs[i]) {
+            case 0x079: i2cBusData[i] = readCam(0); return;
+            case 0x07B: i2cBusData[i] = readCam(1); return;
+            case 0x14B: i2cBusData[i] = readMcu(); return;
+            case 0x179: i2cBusData[i] = readCam(2); return;
+
+        default:
+            // Catch reads from unknown devices
+            LOG_WARN("Unknown I2C bus %d read from device 0x%X\n", i, devAddrs[i]);
+            i2cBusData[i] = 0;
+            return;
+        }
+    }
+
+    // Set the device address on first write
+    i2cBusCnt[i] |= BIT(4); // Acknowledge
+    if (++writeCounts[i] == 1) {
+        devAddrs[i] = i2cBusData[i];
+        return;
+    }
+
+    // Forward I2C writes to the addressed device
+    switch ((i << 8) | devAddrs[i]) {
+        case 0x078: return writeCam(0, i2cBusData[i]);
+        case 0x07A: return writeCam(1, i2cBusData[i]);
+        case 0x14A: return writeMcu(i2cBusData[i]);
+        case 0x178: return writeCam(2, i2cBusData[i]);
+
+    default:
+        // Catch writes to unknown devices
+        LOG_WARN("Unknown I2C bus %d write to device 0x%X\n", i, devAddrs[i]);
+        return;
+    }
 }
